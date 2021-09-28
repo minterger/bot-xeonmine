da5014872e1b1fdeeabe588aab195745a10b91ea
minterger <minterger@gmail.com>
2021-09-28 15:54:22 -0300

    Create getServer.js

---
+++ src/utils/getServer.js
@@ -0,0 +1,11 @@
+const util = require("minecraft-server-util");
+
+const server = async () => {
+  try {
+    return await util.status("play.xeonmine.me", { port: 25565 });
+  } catch (error) {
+    return null;
+  }
+};
+
+module.exports = server;
