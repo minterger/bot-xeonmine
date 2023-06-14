64df373c2103f762797e3d0856d9df0e893f4df2
Leonel Menci <minterger@gmail.com>
2023-06-14 20:31:34 -0300

    add docker suport

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const uri = `mongodb+srv://${process.env.USERNAME_DB}:${process.env.PASSWORD_DB}@${process.env.URL_DB}/${process.env.NAME_DB}?retryWrites=true&w=majority`;
+const uri = process.env.DATABASE;
 
 (async () => {
   try {
